2493faa207fabc41b8f4dee262f1fda14e41f866
hugo riveros <hugo.riveros@itglobers.com>
2023-04-25 02:34:54 -0500

    update

--- src/components/WeddingInvitation/index.tsx
+++ src/components/WeddingInvitation/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import invitados from "../../variables/invitados";
 import IlustrationOne from '../../assets/img/main-page_ilustration1.jpg'
