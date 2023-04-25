ae67d968a23efe48631f19870d2355352fde37b6
hugo riveros <hugo.riveros@itglobers.com>
2023-04-25 02:26:33 -0500

    update

---
+++ src/components/WeddingInvitation/index.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import invitados from "../../variables/invitados";
+import IlustrationOne from '../../assets/img/main-page_ilustration1.jpg'
+import IlustrationTwo from '../../assets/img/main-page_ilustration2.jpg'
+import IlustrationThree from '../../assets/img/main-page_ilustration3.jpg'
+import IlustrationFour from '../../assets/img/main-page_ilustration4.jpg'
+import IlustrationFive from '../../assets/img/main-page_ilustration5.jpg'
+import AngieHugo from '../../assets/img/imagen-angie-hugo.jpg'
+import DressCode from '../../assets/img/dress-code.png'
+import './styles.css';
+
+const WeddingInvitation = () => {
+
+    //PARAMS
+    const { nombreInvitado } = useParams();
+    
+    //JSX
+    return(
+        <>
+            <div className={`main-page__global-container`}>
+                <div className={`main-page__ilustration-one ilustration`}>
+                    <img src={IlustrationOne}/>
+                </div>
+                <div className={`main-page__angie-hugo`}>
+                    <span>ANGIE</span>
+                    <span>&</span>
+                    <span>HUGO</span>
+                </div>
+                <div className={`main-page__ilustration-two ilustration`}>
+                    <img src={IlustrationTwo}/>
+                </div>
+            </div>
+
+            <div className={`content__global-container`}>
+                <div className={`content__invitado`}>
+                    <p>{nombreInvitado ? invitados[nombreInvitado] : ''}</p>
+                </div>
+
+                <div className={`content__text-one`}>
+                    <p>
+                        !NOS CASAMOS! <br/> Y QUEREMOS COMPARTIR CON USTEDES ESTE DÍA TAN ESPECIAL PARA NOSOTROS
+                    </p>
+                </div>
+
+
+                <div className={`content__ceremony-date`}>
+                    <p>03</p>
+                    <p>·</p>
+                    <p>06</p>
+                    <p>·</p>
+                    <p>2023</p>
+                </div>
+
+                <div className={`content__image-one`}>
+                    <img src={IlustrationThree}/>
+                </div>
+
+                <div className={`content__ceremony-info`}>
+                    <div className={`ceremony-info__hour`}>
+                        <h3>CEREMONIA</h3>
+                        <p>HORA: 2:30 PM</p>
+                    </div>
+                    <div className={`ceremony-info__address`}>
+                        <h4>PARROQUIA SANTA MARÍA DEL CAMINO</h4>
+                        <p>Calle 138 # 58d - 35</p>
+                    </div>
+                </div>
+
+                <div className={`content__ceremony-info`}>
+                    <div className={`ceremony-info__hour`}>
+                        <h3>RECEPCIÓN</h3>
+                        <p>HORA: 6:00 PM - 10:00 PM</p>
+                    </div>
+                    <div className={`ceremony-info__address`}>
+                        <h4>RESTAURANTE DANIEL</h4>
+                        <p>Calle 73 # 9 - 70</p>
+                    </div>
+                </div>
+
+                <div className={`content__image-one`}>
+                    <img src={IlustrationFour}/>
+                </div>
+
+                <div className={`content__text-two`}>
+                    <p>POR FAVOR CONFIRMAR LA ASISTENCIA ANTES DEL 15 DE MAYO AL WHATSAPP 3163042458 O 3115292688</p>
+                </div>
+
+
+                <div className={`content__formalities`}>
+                    <div className={`content__formalities--dress`}>
+                        <img src={DressCode}/>
+                        <p>Dress code: formal</p>
+                    </div>
+                    <div className={`content__formalities--sobres`}>
+                        <p>Lluvia de Sobres y Amor ❤️</p>
+                    </div>
+                </div>
+
+                <div className={`content__image-one`}>
+                    <img src={IlustrationFive}/>
+                </div>
+
+                <div className={`content__text-three`}>
+                    <p>"Casarse y compartir la vida es algo hermoso. Es un viaje comprometido, a veces difícil, a veces complicado, pero vale la pena animarse. Y en este viaje de toda la vida, la esposa y el esposo no están solos; los acompaña Jesús"</p>
+                    <p className={`content__text-three--name`}>Papa Francisco</p>
+                </div>
+
+                <div className={`content__image-two`}>
+                    <img src={AngieHugo}/>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default WeddingInvitation
---
+++ src/variables/invitados.ts
@@ -0,0 +1,28 @@
+const invitados:any = {
+    "abuela-nena": "Abuela Nena",
+    "raquel-sandra": "Abuela Raquel & Tía Sandra",
+    "adrian-cami": "Adrian & Cami",
+    "alex-isabel": "Alex & Maria Isabel e Hijo",
+    "andrea": "Andrea",
+    "angie": "Angie",
+    "carolina": "Carolina",
+    "danna-dylan": "Danna & Dylan",
+    "diego-salazar": "Diego Salazar",
+    "enrique-pulido": "Enrique Pulido",
+    "estefania": "Estefania",
+    "harold-melissa": "Harold & Melissa",
+    "jairo-angie": "Jairo & Angie",
+    "joan": "Joan",
+    "jose-natalia": "Jose & Natalia",
+    "juan-camilo": 'Juan Camilo',
+    "leslie-thomi": "Leslie & Thomi",
+    "mama-papa": "Mamá & Papá",
+    "mateo": "Mateo",
+    "nico-vivi": "Nico & Vivi",
+    "priscila-dani": "Tía Priscila & Dani",
+    "rafael-libia-primos": "Rafael, Libia & Primos",
+    "shirley": "Shirley",
+    "william-nataly": "Tío William & Nataly"
+}
+
+export default invitados;
